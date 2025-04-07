25991a0a3f68e668184bb6b4b8637ca2340c0f14
Vijay Singh <thakurvijayofficial@gmail.com>
2025-04-07 20:05:09 +0530

    Added Dark mode to Modal Popup

--- src/Context/Modal.jsx
+++ src/Context/Modal.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./styles/Modal.css";
+import { useMode } from "./ModeContext";
 export default function Modal({ isOpen, onClose, title = "", children }) {
-  if (!isOpen) return null;
+  const { mode } = useMode();
 
+  if (!isOpen) return null;
   return (
     <div className="myModal">
       <div className="row justify-content-center">
-        <div className="bg-white col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5 rounded-lg shadow-lg p-4">
+        <div
+          className={`${
+            mode == "dark" ? "bg-dark text-white" : "bg-white"
+          } col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5 rounded-lg shadow-lg p-4`}
+        >
           <div className="modal-header w-100 pb-2 mb-2 border-bottom">
             <h2 className="text-center m-0">{title}</h2>
             <button className="ms-auto btn" onClick={onClose}>
