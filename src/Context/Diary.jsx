a68b888b270823507854c3bba5110d1d871995ac
Vijay Singh <thakurvijayofficial@gmail.com>
2025-04-07 22:05:54 +0530

    Alert boxes and Diary States

--- src/Context/Diary.jsx
+++ src/Context/Diary.jsx
@@ -3,15 +3,24 @@ import "./styles/Diary.css";
 import Modal from "./Modal";
 import { useState } from "react";
 import { useMode } from "./ModeContext";
+import SwalAlert from "../utils/SwalAlert";
+
 export default function Diary() {
   const { mode } = useMode();
   const [isModalOpen, setModalOpen] = useState(false);
   const [selectedDiary, setSelectedDiary] = useState("");
+  const [diaryData, setDiaryData] = useState(
+    Object.entries(JSON.parse(localStorage.getItem("notely-diary")) || {})
+  );
+
   function saveDiaryEntry(event) {
     const today = new Date().toISOString().split("T")[0];
     const diaryData = JSON.parse(localStorage.getItem("notely-diary")) || {};
     diaryData[today] = event.target.value;
     localStorage.setItem("notely-diary", JSON.stringify(diaryData));
+    setDiaryData(
+      Object.entries(JSON.parse(localStorage.getItem("notely-diary")))
+    );
   }
 
   function openDiaryEntry(date) {
@@ -19,6 +28,25 @@ export default function Diary() {
     setSelectedDiary(diaryData[date]);
     setModalOpen(true);
   }
+
+  function deleteDiaryEntry(date) {
+    SwalAlert({
+      title: "Are you sure?",
+      text: "This diary entry will be deleted.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Delete",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const diaryData = JSON.parse(localStorage.getItem("notely-diary"));
+        delete diaryData[date];
+        localStorage.setItem("notely-diary", JSON.stringify(diaryData));
+        setDiaryData(Object.entries(diaryData));
+        document.getElementById("diary-input").value = "";
+      }
+    });
+  }
+
   return (
     <div className="diary-section">
       <h1 className="diary-title">Diary</h1>
@@ -27,6 +55,7 @@ export default function Diary() {
         experiences here. Your diary is private and only you can see it.
       </p>
       <textarea
+        id="diary-input"
         rows="8"
         className={`diary-input form-control ${
           mode === "night" ? "bg-dark text-light" : ""
@@ -42,8 +71,7 @@ export default function Diary() {
         autoCorrect="off"
         spellCheck="false"
       ></textarea>
-      {Object.entries(JSON.parse(localStorage.getItem("notely-diary")) || {})
-        .length <= 0 ? (
+      {diaryData.length <= 0 ? (
         ""
       ) : (
         <div className="diary-history mt-5 card">
@@ -52,64 +80,48 @@ export default function Diary() {
           </div>
           <div className="card-body">
             <div className="diary-history-list">
-              {Object.entries(
-                JSON.parse(localStorage.getItem("notely-diary")) || {}
-              )
-                .reverse()
-                .map(([date, entry]) => {
-                  const x = new Date(date);
-                  const dateFormatted = `${x.getDate()} ${x.toLocaleString(
-                    "default",
-                    { month: "long" }
-                  )}`;
-                  return (
-                    <div key={date} className="diary-history-item">
-                      <div className="inner-container">
-                        <div className="diary-history-date">
-                          {dateFormatted}
-                        </div>
-                        <div className="short-entry">
-                          {entry.length > 200
-                            ? entry.slice(0, 200) + "..."
-                            : entry}
-                        </div>
-                        <div className="diary-history-actions">
-                          {entry.length > 200 ? (
-                            <div className="diary-history-full-entry">
-                              <button
-                                onClick={() => openDiaryEntry(date)}
-                                className="diary-history-link btn btn-info mb-2"
-                              >
-                                <span className="bi bi-eye"></span>
-                              </button>
-                            </div>
-                          ) : (
-                            ""
-                          )}
-                          <div className="diary-history-delete">
-                            <a
-                              className="diary-history-delete btn btn-danger"
-                              href="#"
-                              onClick={() => {
-                                const diaryData = JSON.parse(
-                                  localStorage.getItem("notely-diary")
-                                );
-                                delete diaryData[date];
-                                localStorage.setItem(
-                                  "notely-diary",
-                                  JSON.stringify(diaryData)
-                                );
-                                window.location.reload();
-                              }}
+              {diaryData.reverse().map(([date, entry]) => {
+                const x = new Date(date);
+                const dateFormatted = `${x.getDate()} ${x.toLocaleString(
+                  "default",
+                  { month: "long" }
+                )}`;
+                return (
+                  <div key={date} className="diary-history-item">
+                    <div className="inner-container">
+                      <div className="diary-history-date">{dateFormatted}</div>
+                      <div className="short-entry">
+                        {entry.length > 200
+                          ? entry.slice(0, 200) + "..."
+                          : entry}
+                      </div>
+                      <div className="diary-history-actions">
+                        {entry.length > 200 ? (
+                          <div className="diary-history-full-entry">
+                            <button
+                              onClick={() => openDiaryEntry(date)}
+                              className="diary-history-link btn btn-info mb-2"
                             >
-                              <span className="bi bi-trash"></span>
-                            </a>
+                              <span className="bi bi-eye"></span>
+                            </button>
                           </div>
+                        ) : (
+                          ""
+                        )}
+                        <div className="diary-history-delete">
+                          <a
+                            className="diary-history-delete btn btn-danger"
+                            href="#"
+                            onClick={() => deleteDiaryEntry(date)}
+                          >
+                            <span className="bi bi-trash"></span>
+                          </a>
                         </div>
                       </div>
                     </div>
-                  );
-                })}
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
