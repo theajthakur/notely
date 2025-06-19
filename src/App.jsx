a69e7bbce10e8dc729e222e0c81a8c3fafc7a100
Vijay Singh <thakurvijayofficial@gmail.com>
2025-06-19 23:04:39 +0530

    Parsing Mode class in APp

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
 
   return (
     <>
-      <div className={`app`}>
+      <div className={`app ${mode}`}>
         <Navbar />
         <Routes>
           <Route path="" element={<Home />} />
